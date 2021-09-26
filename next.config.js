b9f5fdbf011883dd53a70ad2a7a1bcb377d8eb27
Nicolanasr <nasr528@gmail.com>
2021-09-26 22:39:37 +0300

    added rewrites to next config for certs, removed console log

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["i.imgur.com"],
-  },
-}
+	reactStrictMode: true,
+	images: {
+		domains: ["i.imgur.com"],
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/certification/udacity/Full-Stack-Development-Track",
+				destination: "https://s3-us-west-2.amazonaws.com/udacity-printer/production/certificates/9839e02a-2422-4dcb-a563-ec2239a13aed.pdf",
+			},
+			{
+				source: "/certification/coursera/Web-Application-Technologies-and-Django",
+				destination: "https://www.coursera.org/account/accomplishments/certificate/KCJAC7NPH3PL",
+			},
+			{
+				source: "/certification/coursera/Python-for-Everybody-Specialization",
+				destination: "https://www.coursera.org/account/accomplishments/specialization/certificate/PM27BFAX7VDH",
+			},
+		];
+	},
+};
